15a5b38bc54c08069ffe91600d26b777df97870b
joker-mik <48585421+joker-mik@users.noreply.github.com>
2026-09-18 12:44:57 +0200

    rtc: reuse second value within each scheduler tick

--- src/main.c
+++ src/main.c
@@ -308,7 +308,8 @@ int __attribute__ ((noreturn)) main(void)
 				COM_putchar('*');
 				COM_flush();
 #endif
-				bool minute = (RTC_GetSecond() == 0);
+				uint8_t second = RTC_GetSecond();
+				bool minute = (second == 0);
 				CTL_update(minute);
 				if (minute)
 				{
@@ -351,18 +352,18 @@ int __attribute__ ((noreturn)) main(void)
 #if RFM
 				if (rfm_available && (config.RFM_devaddr != 0) && (time_sync_tmo > 1))
 				{
-					if (((RTC_GetSecond() == config.RFM_devaddr) && (wireless_buf_ptr)) ||
+					if (((second == config.RFM_devaddr) && (wireless_buf_ptr)) ||
 					    (
 						    (
-							    (RTC_GetSecond() > 30) &&
+							    (second > 30) &&
 							    (
-								    (RTC_GetSecond() & 1)
+								    (second & 1)
 								    ? (wl_force_addr1 == config.RFM_devaddr)
 								    : (wl_force_addr2 == config.RFM_devaddr)
 							    )
 						    ) || (
 							    (wl_force_addr1 == 0xff) &&
-							    (RTC_GetSecond() % 30 == config.RFM_devaddr) &&
+							    (second % 30 == config.RFM_devaddr) &&
 							    ((wl_force_flags >> config.RFM_devaddr) & 1)
 						    )
 					    )
@@ -371,7 +372,7 @@ int __attribute__ ((noreturn)) main(void)
 						wirelessTimerCase = WL_TIMER_FIRST;
 						RTC_timer_set(RTC_TIMER_RFM, WLTIME_START);
 					}
-					if ((RTC_GetSecond() == 59) || (RTC_GetSecond() == 29))
+					if ((second == 59) || (second == 29))
 					{
 #if (WL_SKIP_SYNC)
 						if (wl_skip_sync != 0)
